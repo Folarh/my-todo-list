24aded5754a4c9c52b648a3be33f41e72fadeb71
Folarh <98031422+Folarh@users.noreply.github.com>
2022-05-07 22:36:16 +0100

    Add files via upload

---
+++ my-todo/original-todo.js
@@ -0,0 +1,38 @@
+const addForm=document.querySelector(".add");
+const list=document.querySelector(".todos")
+
+
+const generateTemplate= (todo)=>{
+const html=`
+<li class="list-group-item d-flex justify-content-between align-items-center">
+<span>${todo}</span>
+<i class="far fa-trash-alt delete"></i>
+</li>`
+
+list.innerHTML+=html;
+}
+
+// adding todos
+
+addForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+
+    const todo= addForm.add.value.trim();
+
+    if(todo.length){
+    generateTemplate(todo);
+    addForm.reset();
+    }
+})
+
+
+
+// deleting todos using icon
+list.addEventListener("click", (e)=>{
+    if(e.target.classList.contains("delete")){
+        e.target.parentElement.remove();
+    }
+})
+
+
+
